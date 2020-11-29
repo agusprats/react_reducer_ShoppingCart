2852161cf2096cf26ca8985b7d8bd437edb9dd34
Agustina Prats <agusprats@gmail.com>
2020-11-29 13:05:31 -0300

    first commit

---
+++ build/precache-manifest.e92da1ba5b7f622574454e8e38f8c453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56faab3f89bb1c251635cb07691ffbae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e51cd0294d640658d552",
+    "url": "/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51cd0294d640658d552",
+    "url": "/static/js/main.331e06ff.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
